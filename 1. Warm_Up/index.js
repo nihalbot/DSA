b7182277bf5101b7587192685f23f22df5da744b
Nihal <nihal@MacBooks-MacBook-Air.local>
2025-08-03 01:15:12 +0700

    first commit

---
+++ 1. Warm_Up/index.js
@@ -0,0 +1,92 @@
+// const a = "Hello There";
+// console.log(a);
+
+// let arr = [7, 5, 6, 7, 8, [2, 4]];
+// arr.map((item)=>(
+//     console.log(item)
+// ));
+
+// let obj = {
+//     name: "Tanvir",
+//     age: 20,
+//     gender: "Male",
+// };
+
+// console.log(obj.name + " " + obj.age + " " + obj.gender);
+//console.log(arr[5][0]);
+
+// Functions
+
+// function printHelloWorld(){
+//     console.log("Hello World");
+// }
+
+// printHelloWorld();
+
+// function greet(name){
+//     console.log("Namaste" + " " + name);
+// }
+
+// greet("Nihal");
+
+// function sum(num1, num2){
+//     const sum = (num1 + num2);
+//     console.log(sum);
+// }
+
+// sum(5, 10);
+
+// multiplication function
+
+// function multiplication(num1, num2){
+//     const result = (num1 * num2);
+//     console.log(result);
+// }
+// multiplication(5, 6);
+
+// square of a number
+
+// function square(number){
+//     return (number * number);
+// }
+// const result = square(9);
+// console.log(result);
+
+// Create a function which accepts the age and tells whether a person is eligible to vot or not
+
+// function eligibleToVote(age){
+//     if(age >= 18){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// const age = 8;
+
+// const result = eligibleToVote(age);
+
+// if(result){
+//     console.log("You are eligible to vote");
+// }else{
+//     console.log("You are not eligible to vote");
+// }
+
+// Create a function to cheek if a number is Even or Odd
+
+function isEvenOdd(number){
+    if(number % 2 == 0){
+        console.log(number + " " + "The number is Even");
+    }else{
+        console.log(number + " " + "The number is Odd");
+    }
+}
+
+isEvenOdd(4);
+// If- Else
+
+// Loop
+  // - for
+  // - while
+
+// Some Program
