f7dc77baf42c8b5e3ba0d631444c626645695e02
Nihal <nihal@MacBooks-MacBook-Air.local>
2025-08-10 23:36:11 +0700

    warm up is done

--- 1. Warm_Up/index.js
+++ 1. Warm_Up/index.js
@@ -86,10 +86,10 @@
 // If- Else =>^
 
 // Loop
-  // - for
-  // - while
+// - for
+// - while
 
-  // for loop
+// for loop
 //   for(let i = 0; i < 20; i++){
 //     console.log(i);
 //   }
@@ -120,13 +120,13 @@
 // }
 
 // Print all the even numbers in the array
-    // let arr = [10, 5, 2, 0, 8, 3];
+// let arr = [10, 5, 2, 0, 8, 3];
 
-    // for(let i = 0; i < arr.length; i++){
-    //     if(arr[i] % 2 == 0){
-    //         console.log(arr[i] + " is a Even number");
-    //     }
-    // }
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] % 2 == 0){
+//         console.log(arr[i] + " is a Even number");
+//     }
+// }
 // Some Program
 
 // Write a function that searches for an element in an array and return the index, if the element is not present then just return -1
@@ -161,7 +161,6 @@
 
 // console.log(result);
 
-
 // Write a function that returns the number of negative numbers in an array
 
 // function findNegativeNumber(arr){
@@ -195,13 +194,10 @@
 
 // Write a function that returns the largest number in an array
 
-
-
-
 // Second Approach
 // function findTheLargest(arr){
 //     let largestNumber = arr[0];
-    
+
 //     for(let i = 0; i < (arr.length); i++){
 //         if(arr[i+1] > arr[i]){
 //             largestNumber = arr[i+1];
@@ -225,7 +221,6 @@
 // let result = findTheLargest(arr);
 // console.log(result);
 
-
 // Write a function that returns the minimum number in an array
 
 // function findTheMinimum(arr){
@@ -244,3 +239,353 @@
 // const result = findTheMinimum(arr);
 
 // console.log(result);
+
+// Find the second largest number in an array
+
+// function findTheSecondLargest(arr) {
+//   if (arr.length < 2) {
+//     console.log(
+//       "Array must contain at least to element to find second largest"
+//     );
+//     return;
+//   }
+
+//   let firstLargest = -Infinity;
+//   let secondLargest = -Infinity;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > firstLargest) {
+//       secondLargest = firstLargest;
+//       firstLargest = arr[i];
+//     } else if (arr[i] > secondLargest && arr[i] != firstLargest) {
+//       secondLargest = arr[i];
+//     }
+//   }
+//   return secondLargest;
+// }
+
+// for find second minimum number
+// function findTheSecondMinimum(arr) {
+//   if (arr.length < 2) {
+//     console.log(
+//       "To find the second minimum number array need at least 2 elements"
+//     );
+//     return;
+//   }
+
+//   let minimunNumber = Infinity;
+//   let secondMinimum = Infinity;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < minimunNumber) {
+//       secondMinimum = minimunNumber;
+//       minimunNumber = arr[i];
+//     } else if (arr[i] < secondMinimum && arr[i] != minimunNumber) {
+//       secondMinimum = arr[i];
+//     }
+//   }
+//   return secondMinimum;
+// }
+
+// let arr = [4, 9, 0, 2, 8, 7, 10];
+
+// const result = findTheSecondMinimum(arr);
+// console.log(result);
+
+// nested loop
+// let counter = 0;
+//  for(let i = 0; i < 3; i++){
+//   for(let j = 0; j < 3; j++){
+//     console.log(counter + 1);
+//     counter++;
+//   }
+//  }
+
+// for(let i = 0; i < 3; i++){
+//   for(let j = 0; j < i; j++){
+//     console.log("i=", i, "  j=", j);
+//   }
+// }
+
+// for(let i = 0; i < 3; i++){
+//   for(let j = i; j >= 0; j--){
+//     console.log("i=", i, "  j=", j);
+//   }
+// }
+// for(let i = 5; i > 0; i--){
+//   for(let j = 0; j < i; j++){
+//     console.log("i="+ i + "  j=" + j);
+//   }
+// }
+
+// Print like this
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+// for(let i = 0; i < 5; i++){
+//   for(let j = 0; j < 5; j++){
+//     process.stdout.write(`${"* "}`);
+//   }
+//   console.log()
+// }
+
+// for(let i = 0; i < 5; i++){
+//   for(let j = 0; j <= i; j++){
+//     process.stdout.write(`${j+1}` + " ");
+//   }
+//   console.log()
+// }
+
+// for(let i = 5; i > 0; i--){
+//   for(let j = 0; j < i; j++){
+//     process.stdout.write(`${"*"}` + " ");
+//   }
+//   console.log();
+// }
+
+// for(let i = 1; i <= 6; i++){
+//   for(let j = 1; j <= i; j++){
+//     if(j % 2 != 0){
+//       process.stdout.write(`${1 }` + " ");
+//     }else{
+//       process.stdout.write(`${0 }` +  " ");
+//     }
+//   }
+//   console.log()
+// }
+
+// for(let i = 0; i < 5; i++){
+//   for(let j = 0; j <= i; j++){
+//     process.stdout.write(`${j+1}` + " ");
+//   }
+//   console.log()
+// }
+
+// let t = 0;
+// for (let i = 5; i > 0; i--) {
+//   //space
+//   for (let j = 1; j < i; j++) {
+//     process.stdout.write(`${" "}`);
+//   }
+//   for (let k = 0; k <= t; k++) {
+//     process.stdout.write(`${"*"}`);
+//   }
+//   t++;
+//   console.log();
+// }
+
+// for (let i = 0; i < 5; i++) {
+//   //space
+//   for (let j = 5; j > i+1; j--) {
+//     process.stdout.write(`${" "}`);
+//   }
+//   for (let k = 0; k <= i; k++) {
+//     process.stdout.write(`${"*"}`);
+//   }
+//   console.log();
+// }
+
+// for(let i = 0; i < n; i++){
+//   let row = '';
+//   for(let j = 0; j < n; j++){
+//       row += '* ';
+//   }
+
+//   console.log(row);
+// }
+
+// for( let i = 0; i < n; i++){
+//   let print = "";
+//   for(let j = 0; j <= i; j++){
+//     print += "* ";
+//   }
+//   console.log(print);
+// }
+// const n = 5;
+
+// for (let i = 0; i < n; i++) {
+//   let print = "";
+//   for (let j = 0; j <= i; j++) {
+//     print += j + 1 + " ";
+//   }
+//   console.log(print);
+// }
+
+// for (let i = 0; i < n; i++) {
+//   let print = "";
+//   for (let j = 0; j <= i; j++) {
+//     print += i + 1 + " ";
+//   }
+//   console.log(print);
+// }
+
+// for (let i = 5; i > 0; i--) {
+//   let print = "";
+//   for (let j = 0; j < i; j++) {
+//     print += j + 1 + " ";
+//   }
+//   console.log(print);
+// }
+
+// for (let i = 0; i < n; i++) {
+//   let print = "";
+//   for (let j = 0; j < n-i; j++) {
+//     print += j + 1 + " ";
+//   }
+//   console.log(print);
+// }
+
+// for(let i = 0; i < n; i++){
+//   let print = "";
+//   //space
+//   for(let j = 0; j < (n - i - 1); j++){
+//     print += " ";
+//   }
+//   for(let k = 0; k <= i; k++){
+//     print += "*";
+//   }
+//   console.log(print);
+// }
+
+// for (let i = 0; i < n; i++) {
+//   let toggle = true;
+//   let print = "";
+//   for (let j = 0; j <= i; j++) {
+//     print += toggle ? "1 " : "0 ";
+
+//     if(toggle){
+//       toggle = false;
+//     }else{
+//       toggle = true;
+//     }
+//   }
+//   console.log(print);
+// }
+
+// let toggle = true;
+
+// for (let i = 0; i < n; i++) {
+//   let print = "";
+//   for (let j = 0; j <= i; j++) {
+//     print += toggle ? "1 " : "0 ";
+
+//     if (toggle) {
+//       toggle = false;
+//     } else {
+//       toggle = true;
+//     }
+//   }
+//   console.log(print);
+// }
+
+// while(number > 0){
+//   number = (number / 10);
+//   number = Math.floor(number)
+//   count++;
+// }
+
+// let number = 0;
+
+// function countDigit(number) {
+//   let count = 0;
+
+//   if (number == 0) {
+//     return 1;
+//   }
+
+//   number = Math.abs(number);
+
+//   for (let i = 0; number > 0; i++) {
+//     number = number / 10;
+//     number = Math.floor(number);
+//     count++;
+//   }
+//   return count;
+// }
+
+// const result = countDigit(number);
+
+// console.log(number + " Contains ", result + " Digits");
+
+// function reverseNumber(number) {
+//   let reverseNum = 0;
+//   let counter = 0;
+//   let base = 1;
+//   let tempNum = number;
+//   let orginalNum = number;
+
+//   for (let i = 0; tempNum > 0; i++) {
+//     tempNum = Math.floor(tempNum / 10);
+//     counter++;
+//   }
+
+//   for (let i = 0; i < counter - 1; i++) {
+//     base *= 10;
+//   }
+
+//   for (let i = 0; number > 0; i++) {
+//     let temp = 0;
+//     temp = number % 10;
+//     number = Math.floor(number / 10);
+//     reverseNum += base * temp;
+//     base = Math.floor(base / 10);
+//   }
+
+//   if (orginalNum == reverseNum) {
+//     console.log("The number is palindrom");
+//   } else {
+//     console.log("The number is not a palindrom");
+//   }
+// }
+
+// //Another Approach
+// function findPallindrom(number){
+//   let reverseNumber = 0;
+//   let orginalNum = number;
+//   for(let i = 0; number > 0; i++){
+//     let reminder = 0;
+//     reminder = (number % 10);
+//     number = Math.floor((number / 10));
+//     reverseNumber = ((10 * reverseNumber) + reminder);
+//   }
+  
+//   if(orginalNum == reverseNumber){
+//     console.log("The number is pallindrom");
+//   } else{
+//     console.log("The number is not a pallindrom");
+//   }
+// }
+
+// let number = 121;
+// const result = findPallindrom(number);
+
+// if(result){
+//   console.log("")
+// }
+
+var reverse = function (x) {
+  let orginalNumber = x;
+  let reverseNumber = 0;
+  x = Math.abs(x);
+
+  for (let i = 0; x > 0; i++) {
+    let reminder = 0;
+    reminder = x % 10;
+    x = Math.floor(x / 10);
+    reverseNumber = (10 * reverseNumber) + reminder;
+  }
+
+  if (reverseNumber > Math.pow(2, 31) || reverseNumber < Math.pow(-2, 31)) {
+    return 0;
+  } 
+
+  return (orginalNumber < 0) ? -reverseNumber: reverseNumber;
+};
+
+let number = 123;
+
+let result = reverse(number);
+console.log(result);
